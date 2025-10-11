65bd024eb07f356df37949737aa0d1731a3a7499
Denis Klintukhov <denis.klintukhov@gmail.com>
2025-10-11 19:59:46 +0300

    Add: Errors handling

--- src/main.rs
+++ src/main.rs
@@ -17,25 +17,31 @@
  *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
+use serde_json;
+use std::env;
 use std::fs;
-use std::io;
 use teloxide::{
     prelude::*,
     types::{KeyboardButton, KeyboardMarkup},
 };
-use serde_json;
 mod models;
-use models::{MenuConfig};
+use models::MenuConfig;
 
 static MENU_CONFIG: &str = "menu.json";
 
 #[tokio::main]
 async fn main() {
-    // dotenvy::dotenv().ok();
-    // pretty_env_logger::init();
+    dotenvy::dotenv().ok();
+    pretty_env_logger::init();
 
     //let bot = Bot::from_env();
-    let json_str = fs::read_to_string(MENU_CONFIG).unwrap();
-    let res = serde_json::from_str::<MenuConfig>(&json_str);
-    println!("{:?}", res);
+    let menu_conf_path = env::current_exe()
+        .expect("Failed to get the app path.")
+        .parent()
+        .expect("Failed to get the app directory.")
+        .join(MENU_CONFIG);
+    let json_str = fs::read_to_string(menu_conf_path).expect("Failed to read the config menu.");
+    let menu =
+        serde_json::from_str::<MenuConfig>(&json_str).expect("Failed to parse the config menu.");
+    println!("{:?}", menu);
 }
